9001556efa6e7ebfdf0a583560f9dab3626124df
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-03-15 13:43:40 -0400

    adapted test

--- test_simple_model_salome.py
+++ test_simple_model_salome.py
@@ -1,28 +1,26 @@
 import festim as F
-from fenics import *
-
-from convert_mesh import convert_mesh
 
 
 def run_simple_sim():
-    correspondance_dict = convert_mesh("mesh.med")
 
-    fluid_id = correspondance_dict["fluid"]
-    inlet_id = correspondance_dict["inlet"]
-    outlet_id = correspondance_dict["outlet"]
-    walls_id = correspondance_dict["walls"]
-
+    # TODO find a way to make this more generic
+    fluid_id = 6
+    inlet_id = 7
+    outlet_id = 8
     my_sim = F.Simulation()
 
     my_sim.mesh = F.MeshFromXDMF(
         volume_file="mesh_cells.xdmf", boundary_file="mesh_facets.xdmf"
     )
 
-    my_sim.materials = F.Material(id=fluid_id, D_0=1, E_D=0)
+    my_sim.materials = F.Material(id=fluid_id, D_0=2, E_D=0)
 
     inlet_BC = F.DirichletBC(surfaces=inlet_id, value=1, field="solute")
     outlet_BC = F.DirichletBC(surfaces=outlet_id, value=0, field="solute")
-    my_sim.boundary_conditions = [inlet_BC, outlet_BC]
+    my_sim.boundary_conditions = [
+        inlet_BC,
+        outlet_BC,
+    ]
 
     my_sim.T = F.Temperature(500)
 
