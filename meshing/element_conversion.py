9df79ef06816ed598cd20fb016acdacaa762e226
kaelyndunnell <kaelyndunnell03@gmail.com>
2026-04-04 13:28:05 -0400

    remove BL on inlet/outlet

--- meshing/element_conversion.py
+++ meshing/element_conversion.py
@@ -9,6 +9,14 @@ write_path = "meshing/tes_openfoam.msh"
 mesh = meshio.read(mesh_file_path)
 print("cell types:", [b.type for b in mesh.cells])
 
+QUAD_TO_TRIS = np.array(  # conversion array to make quads --> tris (2 tris per quad)
+    [
+        [0, 1, 2],
+        [0, 2, 3],
+    ],
+    dtype=np.int64,
+)
+
 WEDGE_TO_TETS = np.array(  # conversion array to make wedges --> tets (3 tets per wedge)
     [
         [0, 1, 2, 5],
@@ -19,8 +27,17 @@ WEDGE_TO_TETS = np.array(  # conversion array to make wedges --> tets (3 tets pe
 )
 
 
+def quad_to_tris(quad_conn: np.ndarray) -> np.ndarray:
+    """converts (n,4) quads to (n*2, 3) tris."""
+    n = quad_conn.shape[0]
+    tris = np.empty((n * 2, 3), dtype=np.int64)
+    for i, local in enumerate(QUAD_TO_TRIS):
+        tris[i::2] = quad_conn[:, local]
+    return tris
+
+
 def wedge_cells_to_tets(wedge_conn: np.ndarray) -> np.ndarray:
-    """converts a (n,6) wedge connectivity array to an (n*3, 4) tet connectivity array."""
+    """converts a (n,6) wedge to an (n*3, 4) tet."""
     n = wedge_conn.shape[0]
     tets = np.empty((n * 3, 4), dtype=np.int64)
     for i, local in enumerate(WEDGE_TO_TETS):
@@ -35,40 +52,53 @@ points = mesh.points
 wedge_keys = {"wedge", "wedge6", "penta6"}
 tet_keys = {"tetra", "tetra4", "tet4"}
 tri_keys = {"triangle", "triangle3", "tri3"}
+surf_keys = {"triangle", "triangle3", "tri3", "quad"}
 
 # assigning names to each block idk from cubit
 physical_groups = {
     1: (1, "fluid", 3),
     2: (2, "inlet", 2),
-    3: (3, "outlet", 2),
-    4: (4, "walls", 2),
+    3: (2, "inlet", 2),
+    4: (3, "outlet", 2),
+    5: (3, "outlet", 2),
+    6: (4, "walls", 2),
 }
 
 # SURFACES
-out_cells = []  # output cell data
-out_phys = []  # tagging for gmsh physical groups (also geometry)
+all_tris = []  # output cell data
+all_tris_phys = []  # tagging for gmsh physical groups (also geometry)
 
 for block_idx, cell_block in enumerate(
     mesh.cells
 ):  # block_idx is equal to block id in cubit
 
     ctype = cell_block.type.lower()
-    if ctype not in tri_keys:
+    if ctype not in surf_keys:
         continue
+
+    print(ctype, block_idx)
 
     tag, name, _ = physical_groups[block_idx]
     data = cell_block.data
     n = len(data)
 
-    # assign block_idx as tag
-    phys = np.full(n, block_idx, dtype=np.int32)
-
+    # assign tag based on physical groups
+    phys = np.full(n, tag, dtype=np.int32)
     unique_phys = np.unique(phys)
 
-    print(f"found {n} triangles in block {block_idx}.")
+    if ctype in tri_keys:
+        print(f"found {n} {ctype} in block {block_idx}, {name}.")
+        all_tris.append(("triangle", data))
+        all_tris_phys.append(phys)
 
-    out_cells.append(("triangle", data))
-    out_phys.append(phys)
+    elif "quad" in ctype:
+        converted = quad_to_tris(data)
+        print(
+            f"found {n} quads --> converted to {len(converted)} triangles in block {block_idx}."
+        )
+        phys_rep = np.repeat(phys, 2)
+        all_tris.append(("triangle", converted))
+        all_tris_phys.append(phys_rep)
 
 # VOLUMES
 all_tets = []  # same logic as above
@@ -77,6 +107,7 @@ all_tet_phys = []
 for block_idx, cell_block in enumerate(mesh.cells):
     ctype = cell_block.type.lower()
     data = cell_block.data
+    n = len(data)
 
     def get_vol_tag():  # wedges are given id 0 and tets are given id 1, manually setting all vol ids as 1 as in cubit
         if block_idx == 0:
@@ -107,8 +138,8 @@ combined_tet_phys = np.concatenate(all_tet_phys)
 
 print(f"total tets in new mesh is {len(combined_tets)}.")
 
-out_cells.append(("tetra", combined_tets))
-out_phys.append(combined_tet_phys)
+all_tris.append(("tetra", combined_tets))
+all_tris_phys.append(combined_tet_phys)
 
 field_data = {
     name: np.array([tag, dim]) for _, (tag, name, dim) in physical_groups.items()
@@ -117,13 +148,15 @@ field_data = {
 ### WRITE MESH ###
 out_mesh = meshio.Mesh(
     points=points,
-    cells=out_cells,
+    cells=all_tris,
     cell_data={
-        "gmsh:physical": out_phys,
-        "gmsh:geometrical": out_phys,
+        "gmsh:physical": all_tris_phys,
+        "gmsh:geometrical": all_tris_phys,
     },
     field_data=field_data,
 )
+
+print("new mesh cell types:", [b.type for b in out_mesh.cells])
 
 print(f"writing mesh to {write_path}.")
 meshio.write(write_path, out_mesh, file_format="gmsh22", binary=False)
