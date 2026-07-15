454e0cad45efefae536a98ac00e08a7c7453c726
jhdark <james.dark.95@gmail.com>
2026-07-15 10:40:18 -0400

    revamped to use ew version

--- festim_scripts/openfoam_to_festim.py
+++ festim_scripts/openfoam_to_festim.py
@@ -1,12 +1,7 @@
-import festim as F
 from foam2dolfinx import OpenFOAMReader
-from dolfinx import fem
-from festim.helpers import nmm_interpolate
 from dolfinx.io import VTXWriter
 from mpi4py import MPI
-import numpy as np
-from dolfinx.mesh import meshtags, exterior_facet_indices
-from scipy.spatial import cKDTree
+import io4dolfinx
 
 
 def read_openfoam_data(file_name):
@@ -18,21 +13,37 @@ def read_openfoam_data(file_name):
 
     final_time = max(openfoam_reader.times)
 
-    p = openfoam_reader.create_dolfinx_function(t=final_time, name="p")
-    u = openfoam_reader.create_dolfinx_function(t=final_time, name="U")
+    # p = openfoam_reader.create_dolfinx_function_with_cell_data(t=final_time, name="p")
+    u = openfoam_reader.create_dolfinx_function_with_cell_data(t=final_time, name="U")
     mesh = openfoam_reader.dolfinx_meshes_dict["default"]
 
     try:
         # read turbulent viscosity if it exists
-        nut = openfoam_reader.create_dolfinx_function(t=final_time, name="nut")
+        nut = openfoam_reader.create_dolfinx_function_with_cell_data(
+            t=final_time, name="nut"
+        )
         print("'nut' field found and read.")
     except Exception:
         nut = None
         print("No 'nut' field found.")
 
-    facet_meshtags, volume_meshtags = define_meshtags(openfoam_reader)
+    facet_meshtags = openfoam_reader.create_facet_meshtags()
+    volume_meshtags = openfoam_reader.create_cell_meshtags()
 
-    return p, u, mesh, nut, facet_meshtags, volume_meshtags
+    checkpoint_file = "test_cp.bp"
+    io4dolfinx.write_mesh(checkpoint_file, mesh)
+
+    io4dolfinx.write_function(checkpoint_file, u, time=0.0, name="U")
+    io4dolfinx.write_function(checkpoint_file, nut, time=0.0, name="nut")
+
+    io4dolfinx.write_meshtags(
+        checkpoint_file, mesh, facet_meshtags, meshtag_name="facet_tags"
+    )
+    io4dolfinx.write_meshtags(
+        checkpoint_file, mesh, volume_meshtags, meshtag_name="cell_tags"
+    )
+
+    return u, mesh, nut, facet_meshtags, volume_meshtags
 
 
 def export_openfoam_data(p, u):
@@ -58,74 +69,10 @@ def export_openfoam_data(p, u):
     writer_u.write(t=0)
 
 
-def tag_boundary_patch(dolfinx_mesh, patch_dataset, patch_id, tol=1e-6):
-    fdim = dolfinx_mesh.topology.dim - 1
-    dolfinx_mesh.topology.create_connectivity(fdim, 0)
-    dolfinx_mesh.topology.create_connectivity(0, fdim)
-    dolfinx_mesh.topology.create_connectivity(fdim, dolfinx_mesh.topology.dim)
-
-    facet_indices = exterior_facet_indices(dolfinx_mesh.topology)
-    x = dolfinx_mesh.geometry.x
-    patch_points = patch_dataset.points
-    tree = cKDTree(x)
-    matched_vertex_indices = tree.query_ball_point(patch_points, tol)
-    matched_vertex_indices = list(set(i for sub in matched_vertex_indices for i in sub))
-
-    matched_facets = []
-    for facet in facet_indices:
-        vertices = dolfinx_mesh.topology.connectivity(fdim, 0).links(facet)
-        if all(v in matched_vertex_indices for v in vertices):
-            matched_facets.append(facet)
-
-    # print(f"Tagging {len(matched_facets)} facets for patch ID {patch_id}")
-    return np.array(matched_facets, dtype=np.int32), np.full(
-        len(matched_facets), patch_id, dtype=np.int32
-    )
-
-
-def define_meshtags(cfd_reader):
-    OF_multiblock = cfd_reader.reader.read()
-    boundary = OF_multiblock["boundary"]
-    mesh = cfd_reader.dolfinx_meshes_dict["default"]
-
-    all_facets = np.array([], dtype=np.int32)
-    all_tags = np.array([], dtype=np.int32)
-
-    for i, name in enumerate(boundary.keys()):
-        if name not in ["inlet", "outlet", "wall", "probe"]:
-            continue
-        facets, tags = tag_boundary_patch(mesh, boundary[name], i + 1)
-        all_facets = np.concatenate([all_facets, facets])
-        all_tags = np.concatenate([all_tags, tags])
-
-        # print(f"Tagging {len(facets)} facets for patch {name} with ID {i + 1}")
-
-    facet_tags = meshtags(
-        mesh,
-        mesh.topology.dim - 1,
-        all_facets,
-        all_tags,
-    )
-
-    num_cells = mesh.topology.index_map(mesh.topology.dim).size_local
-    mesh_cell_indices = np.arange(num_cells, dtype=np.int32)
-    tags_volumes = np.full(num_cells, 1, dtype=np.int32)
-    volume_meshtags = meshtags(mesh, mesh.topology.dim, mesh_cell_indices, tags_volumes)
-
-    num_cells = (
-        mesh.topology.index_map(mesh.topology.dim).size_local
-        + mesh.topology.index_map(mesh.topology.dim).num_ghosts
-    )
-
-    print(f"number of cells in mesh: {num_cells}")
-    return (
-        facet_tags,
-        volume_meshtags,
-    )
-
-
 if __name__ == "__main__":
     # read openfoam data
-    p, u = read_openfoam_data("OpenFOAM/probe-case/case.foam")
+    p, u, mesh, nut, facet_meshtags, volume_meshtags = read_openfoam_data(
+        "OpenFOAM/probe-case/case.foam"
+    )
 
     export_openfoam_data(p, u)
